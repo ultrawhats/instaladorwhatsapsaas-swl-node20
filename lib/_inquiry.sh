beb53d5504759ab817695f16afbf0bd4d716a286
UltraWhats <contato@ultrawhats.com.br>
2026-01-19 10:49:53 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -291,7 +291,7 @@ software_dominio() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador PLW DESIGN, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador SWL, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
   printf "   [0] Instalar whaticket\n"
   printf "   [1] Atualizar whaticket\n"
